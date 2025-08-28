dbcaec22929e63065263b9cd25ad27d6eb4ae0ac
bethgrentz <bethanymwilliford@gmail.com>
2025-08-28 14:30:05 -0600

    Updated README and added Jikan API package to randomly select an anime from much greater library of anime recommendations with description

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Anime Decision Maker
 
 * Bethany Grentz
-* 8/##/25
+* 8/28/25
 
 ## Resources Used
 Source of Idea: https://www.youtube.com/watch?v=_xf1TMs0ysk
-
+Jikan API: https://github.com/abhinavk99/jikanpy#documentation
 
 ## Overview
-TBD
+Simple program that asks the user their top favorite anime and randomly picks one. Then digs into Jikan API which interfaces
+with MyAnimeList to grab a random title from an even larger collection of anime titles to choose and make a recommendation.
+
+## Future Goals:
+1. Update the output of titles to be restricted to ratings up to PG 13 (some anime is questionable, I'd like to clean this up)
+2. Make more educated guesses given the list of anime provided by the user (ex: series vs. movie, tailor specific genres, etc.)
+3. Complete more testing to catch more errors
+4. Try searching outside Jikan to get more descriptions for user if there is none
--- practice.py
+++ practice.py
@@ -1,21 +1,41 @@
 # ~ Anime Decision Maker ~
 # 
- #TBD
+# Simple program that asks the user their top favorite anime and randomly picks one. Then digs into Jikan API which interfaces
+# with MyAnimeList to grab a random title from an even larger collection of anime titles to choose and make a recommendation.
 #
 # Author: Bethany Grentz
-# Date: Tbd
+# Date: 8/28/25
 # Version: 1.0
 
 
 # Imports
 import random
 import pandas as pd
+from jikanpy import Jikan
 
 # Collect a list from the user
-print("What are your top 4 favorite anime?")
-list = [input(f"Anime {i+1}: ") for i in range (4)]
+print("\nWhat are your top 6 favorite anime?")
+list = [input(f"Anime {i+1}: ") for i in range (6)]
 print("Great! You listed: ", list)
 
-# Make random selection
+# Make random selection from favorites list
 randomPick = random.choice(list)
 print(f"\n Hmm...Why don't you watch {randomPick} today?")
+
+# Make Jikan Suggestion
+print("\nOrrr if you are feeling adventurous, why don't you try this one?:")
+jikan = Jikan()
+
+# Select Random and Title
+jikanSelectedAnime = jikan.random(type = 'anime')
+animeTitle = jikanSelectedAnime['data']['title']
+print(f"\n{animeTitle}")
+
+# Give the user more information if available
+animeSynopsis = jikanSelectedAnime['data']['synopsis']
+
+# Check if it's available or not
+if animeSynopsis == None:
+    print("Darn, I couldn't find a good description from MyAnimeList source. I'll be working on this issue! :)")
+else:
+    print(f"\nHere is what I could find about this anime: \n{animeSynopsis}")
