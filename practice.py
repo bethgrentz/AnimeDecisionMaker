6395ea576ef9582ad12f31cc5d1db28d70bb34b7
bethgrentz <bethanymwilliford@gmail.com>
2025-08-28 13:44:21 -0600

    Add missing comments to make code cleaner

--- practice.py
+++ practice.py
@@ -13,10 +13,9 @@ import pandas as pd
 
 # Collect a list from the user
 print("What are your top 4 favorite anime?")
-
 list = [input(f"Anime {i+1}: ") for i in range (4)]
-
 print("Great! You listed: ", list)
 
+# Make random selection
 randomPick = random.choice(list)
 print(f"\n Hmm...Why don't you watch {randomPick} today?")
