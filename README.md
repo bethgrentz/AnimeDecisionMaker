3e62d19a1497016d0f92098291be0c0920c16d9f
bethgrentz <bethanymwilliford@gmail.com>
2025-08-28 13:11:44 -0600

    Practice collecting info from user and printing back to user

---
+++ README.md
@@ -0,0 +1,11 @@
+# Anime Decision Maker
+
+* Bethany Grentz
+* 8/##/25
+
+## Resources Used
+Source of Idea: https://www.youtube.com/watch?v=_xf1TMs0ysk
+
+
+## Overview
+TBD
--- practice.py
+++ practice.py
@@ -1,1 +1,22 @@
-#Filler
+# ~ Anime Decision Maker ~
+# 
+ #TBD
+#
+# Author: Bethany Grentz
+# Date: Tbd
+# Version: 1.0
+
+
+# Imports
+import random
+import pandas as pd
+
+# Collect a list from the user
+print("What are your top 4 favorite anime?")
+
+topOne = input()
+topTwo = input()
+topThree = input()
+topFour = input()
+
+print(f"Cool, I like {topOne}, {topTwo}, {topThree}, {topFour} too!")
